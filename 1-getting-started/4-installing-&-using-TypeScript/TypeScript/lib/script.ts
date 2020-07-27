cd960569a5a5268f9b4a56f0a763e414534cb63f
lawrencedovin <lawrence@qwikwire.com>
2020-07-27 11:50:20 -0400

    Completed Installing & Using TypeScript section.

--- 1-getting-started/4-installing-&-using-TypeScript/TypeScript/lib/script.ts
+++ 1-getting-started/4-installing-&-using-TypeScript/TypeScript/lib/script.ts
@@ -1,14 +1,14 @@
 window.onload = function () {
     const button = document.querySelector("button");
-    const input1 = document.querySelector("#num1")!;
-    const input2 = document.querySelector("#num2")!;
-    // ! means that it w
+    const input1 = document.querySelector("#num1")! as HTMLInputElement;
+    const input2 = document.querySelector("#num2")! as HTMLInputElement;
+    // ! as HTMLInputElement is type casting
 
-    function add(num1, num2) {
+    function add(num1: number, num2: number) {
         return num1 + num2;
     }
 
     button.addEventListener('click', function () {
-        console.log(add(input1.value, input2.value));
+        console.log(add(+input1.value, +input2.value));
     });
 }
