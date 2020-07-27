00d8a2a40fd56843e67a24db52cf7763b54ccca9
lawrencedovin <lawrence@qwikwire.com>
2020-07-27 19:48:46 -0400

    Finished type assignment & type inference.

---
+++ 2-typescript-basics-&-basic-types/15-type-assignment-inference/lib/app.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    function add(n1, n2, showResult, phrase) {
+        var result = n1 + n2;
+        if (showResult) {
+            console.log(phrase + result);
+        }
+        else {
+            return result;
+        }
+    }
+    var number1;
+    number1 = 5;
+    var number2 = 2.58;
+    var printResult = true;
+    var resultPhrase = 'Result is: ';
+    // resultPhrase = 0;
+    add(number1, number2, printResult, resultPhrase);
+};
