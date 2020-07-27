e40bb4d25474166a332365796f33cdfec869fc83
lawrencedovin <lawrence@qwikwire.com>
2020-07-27 18:47:43 -0400

    Completed TypeScript types vs JavaScript types section.

--- 2-typescript-basics-&-basic-types/11-using-types/lib/app.ts
+++ 2-typescript-basics-&-basic-types/11-using-types/lib/app.ts
@@ -1,6 +1,10 @@
 window.onload = function () {
   function add(n1: number, n2: number) {
-    return n1 + n2;
+      if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('incorrect input!');
+      } else {
+          return +n1 + +n2;
+      }
   }
 
   const number1 = 5;
