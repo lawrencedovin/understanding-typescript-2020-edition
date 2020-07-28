7004f4a4e4c48be5b3cd7c74e1755b7df7866d53
lawrencedovin <lawrence@qwikwire.com>
2020-07-28 14:14:42 -0400

    Completed object types section.

---
+++ 2-typescript-basics-&-basic-types/16-object-types/lib/app.ts
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+  // const person: {
+  //   name: string;
+  //   age: number;
+  // } = {
+  const person = {
+    name: 'Lawrence',
+    age: 30
+  };
+
+  console.log(person.name);
+}
